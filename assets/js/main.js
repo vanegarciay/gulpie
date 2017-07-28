e3db32aad24f86beac907b75a01f4dcd16edbdd4
VaneGarciaYara <vanegarciay@gmail.com>
2017-07-27 23:23:50 -0400

    done

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    //side nav bar
+    $('.button-collapse').sideNav({
+        menuWidth: 170,
+        edge: 'left',
+        closeOnClick: true,
+        draggable: true
+    });
+});
